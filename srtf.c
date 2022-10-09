bf02f9efa5aba81cf66d74c86addd3295bd729c8
abhi2666 <95623413+abhi2666@users.noreply.github.com>
2022-10-09 21:41:22 +0530

    commit

--- srtf.c
+++ srtf.c
@@ -1,43 +1,66 @@
-// SRTF
 #include <stdio.h>
-int main()
-{
-    int at[10],bt[10],rt[10],endTime,i,smallest;
-    int remain=0,n,time,sum_wait=0,sum_turnaround=0;
-    printf("Enter no of Processes : ");
-    scanf("%d",&n);
-    for(i=0;i < n; i++)
-    {
-        printf("Enter arrival time for Process P%d : ",i+1);
-        scanf("%d",&at[i]);
-        printf("Enter burst time for Process P%d : ",i+1);
-        scanf("%d",&bt[i]);
-        rt[i]=bt[i];
 
-    }
-    printf("\n\nProcess\t|Turnaround Time| Waiting Time\n\n");
-    rt[9]= {9999};
-    for(time=0;remain!=n;time++)
+void findTimes(int at[], int bt[], int wt[], int tat[], int n)
+{
+    int rem_bt[n];
+    for (int i = 0; i < n; i++)
     {
-        smallest=9;
-        for(i=0;i
+        rem_bt[i] = bt[i];
+    }
+    int remaining = 0, time, smallest = 0, CT = 0; // obviously !!
+
+    for(time = 0; remaining != n; time++)
+    {
+        // time will increase and we will check process 
+        // with shortest burst time each time.
+
+        for(int i = 0; i < n; i++)
         {
-            if(at[i]<=time && rt[i]0)
+            // check if any process is ready to be executed
+            // also check if its bt is not zero
+            if(at[i] <= time && rem_bt[i])
             {
-                smallest=i;
+                // find the shortest process
+                smallest = i; // store its index
             }
         }
-        rt[smallest]--;
-        if(rt[smallest]==0)
+        rem_bt[smallest]--;
+        // if process is already fully executed then
+        // calcuate its waiting time and TAT
+        if(rem_bt[smallest] == 0)
         {
-            remain++;
-            endTime=time+1;
-            printf("\nP[%d]\t|\t%d\t|\t%d",smallest+1,endTime-at[smallest],endTime-bt[smallest]-at[smallest]);
-            sum_wait+=endTime-bt[smallest]-at[smallest];
-            sum_turnaround+=endTime-at[smallest];
+            remaining++; // one process is completed
+            CT = time + 1; // completion time
+            printf("For Process %d\n", smallest+1);
+            printf("Waiting Time is : %d\n", CT - at[smallest] - bt[smallest]);
+            printf("Turn Around Time : %d\n", CT - at[smallest]);
         }
     }
-    printf("\n\nAverage waiting time = %f\n",sum_wait*1.0/n);
-    printf("Average Turnaround time = %f",sum_turnaround*1.0/5);
+    
+}
+
+int main()
+{
+    int n;
+    printf("Enter no. of processes : \n");
+    scanf("%d", &n);
+    int at[n], bt[n], wt[n], tat[n];
+
+    printf("Enter Arrival Time: \n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &at[i]);
+    }
+
+    printf("Enter Burst Time: \n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &bt[i]);
+    }
+
+    findTimes(at, bt, wt, tat, n);
+    
+    
+
     return 0;
 }
